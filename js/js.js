64fdc3a182ac791e66d5d1f6f5077afa06bc65f3
blysenko <lysenko.boris25@gmail.com>
2018-10-04 09:20:29 +0300

    Change
    New change

--- js/js.js
+++ js/js.js
@@ -1,140 +1,141 @@
-function CellTable(property) {
+class CellTable {
 
-    var _this = this;
+    constructor(property) {
 
-    var defaultProperty = {
-        "table": '#cell-table',
-        "addColumn": '#add-column',
-        "addRow": '#add-row',
-        "removeRow": '#remove-row',
-        "removeColumn": '#remove-col',
-        "hover": '.hover'
-    };
+        let defaultProperty = {
+            "table": '.cell-table',
+            "addColumnButton": '.add-column',
+            "addRowButton": '.add-row',
+            "removeRowButton": '.remove-row',
+            "removeColumnButton": '.remove-col',
+            "hover": '.hover'
+        };
 
-    property = Object.assign(defaultProperty, property || {});
+        property = Object.assign(defaultProperty, property || {});
 
-    this.hover = document.querySelectorAll(property.hover);
-    this.table = document.querySelector(property.table);
-    this.addColumn = document.querySelector(property.addColumn);
-    this.addRow = document.querySelector(property.addRow);
-    this.removeRow = document.querySelector(property.removeRow);
-    this.removeColumn = document.querySelector(property.removeColumn);
 
-    if (_this.table) {
-        this.addColumn.addEventListener('click', function () {
-            addCol(_this.table);
-        });
+        this.hover = document.querySelectorAll(property.hover);
+        this.table = document.querySelector(property.table);
+        this.addColumnButton = document.querySelector(property.addColumnButton);
+        this.addRowButton = document.querySelector(property.addRowButton);
+        this.removeRowButton = document.querySelector(property.removeRowButton);
+        this.removeColumnButton = document.querySelector(property.removeColumnButton);
 
-        this.addRow.addEventListener('click', function () {
-            addRow(_this.table);
-        });
 
-        this.removeColumn.addEventListener('click', function () {
-            removeColumn(_this.removeColumn.getAttribute('data-table'))
-        });
+        if (this.table) {
+            this.addColumnButton.addEventListener('click', () => {
+                this.addCol(this.table);
+            });
 
-        this.removeRow.addEventListener('click', function () {
-            removeRow(_this.removeRow.getAttribute('data-table'));
-        });
+            this.addRowButton.addEventListener('click', () => {
+                this.addRow(this.table);
+            });
 
-        this.table.tBodies[0].addEventListener('mouseover', function (e) {
-            var cellTD = document.elementFromPoint(e.clientX, e.clientY);
-            var coords = cellTD.getBoundingClientRect();
-                var RowIndex = cellTD.parentNode.rowIndex;
-                var CellIndex = cellTD.cellIndex;
-                _this.removeColumn.setAttribute('data-table', CellIndex);
-                _this.removeRow.setAttribute('data-table', RowIndex);
-            if (cellTD == '[object HTMLTableCellElement]') {
-                buttonsPosition(coords.top+window.pageYOffset, coords.left+window.pageXOffset);
+            this.removeColumnButton.addEventListener('click', () => {
+                this.removeColumn(this.removeColumnButton.getAttribute('data-table'))
+            });
+
+            this.removeRowButton.addEventListener('click', () => {
+                this.removeRow(this.removeRowButton.getAttribute('data-table'));
+            });
+
+            this.table.tBodies[0].addEventListener('mouseover', (e) => {
+                let cellTD = document.elementFromPoint(e.clientX, e.clientY);
+                let coords = cellTD.getBoundingClientRect();
+                let RowIndex = cellTD.parentNode.rowIndex;
+                let CellIndex = cellTD.cellIndex;
+                this.removeColumnButton.setAttribute('data-table', CellIndex);
+                this.removeRowButton.setAttribute('data-table', RowIndex);
+                if (cellTD == '[object HTMLTableCellElement]') {
+                    this.buttonsPosition(coords.top + window.pageYOffset, coords.left + window.pageXOffset);
+                }
+            });
+
+            for (let i = 0; i < this.hover.length; i++) {
+                this.hover[i].addEventListener('mouseover', () => {
+                    let cellsLength = this.table.rows[0].cells.length;
+                    let rowLength = this.table.rows.length;
+                    if (cellsLength <= 1) {
+                        this.removeColumnButton.style.display = 'none';
+                    } else {
+                        this.removeColumnButton.style.display = 'block';
+                    }
+                    if (rowLength <= 1) {
+                        this.removeRowButton.style.display = 'none';
+                    } else {
+                        this.removeRowButton.style.display = 'block';
+                    }
+                });
+                this.hover[i].addEventListener('mouseout', () => {
+                    this.removeRowButton.style.display = 'none';
+                    this.removeColumnButton.style.display = 'none';
+                });
             }
-        });
-
-        for (i = 0; i < _this.hover.length; i++){
-            _this.hover[i].addEventListener('mouseover', function () {
-                var cellsLength = _this.table.rows[0].cells.length;
-                var rowLength = _this.table.rows.length;
-                if(cellsLength <= 1){
-                    _this.removeColumn.style.display = 'none';
-                } else {
-                    _this.removeColumn.style.display = 'block';
-                }
-                if (rowLength <= 1){
-                    _this.removeRow.style.display = 'none';
-                } else {
-                    _this.removeRow.style.display = 'block';
-                }
-            })
-            _this.hover[i].addEventListener('mouseout', function (){
-                _this.removeRow.style.display = 'none';
-                _this.removeColumn.style.display = 'none';
-            })
         }
     }
 
-    function removeColumn(cellIndex) {
-        var Collumns = _this.table.tBodies[0].rows; 
-        var rowsLength = _this.table.tBodies[0].rows.length;
-        var removeColumnLeft = Collumns[0].cells[0].getBoundingClientRect();
-        var defaultCoordLeft = removeColumnLeft.left + window.pageXOffset;
-        for (var i = 0; i < rowsLength; i++ ){
-            if(_this.table.tBodies[0].rows[i].cells.length !== 1){
-                _this.removeColumn.style.display = 'none';
-                _this.removeColumn.style.left = defaultCoordLeft + 'px';
-                removeCol(cellIndex, Collumns[i]);
-            } 
-        }
-        
-    }
 
-    function removeRow(rowIndex) {
-         var Collumns = _this.table.tBodies[0];
-         var cellsLength = _this.table.tBodies[0].rows[0].cells.length;
-         var removeRowTop = Collumns.rows[0].cells[0].getBoundingClientRect();
-         var defaultCoordsTop = removeRowTop.top + window.pageXOffset;
-         console.log(rowIndex);
-            if(_this.table.rows.length !== 1){
-                _this.removeRow.style.display = 'none';
-                _this.removeRow.style.top = defaultCoordsTop + 'px' ;
-                RemoveRow(Collumns, rowIndex);
+    removeColumn(cellIndex) {
+        let Collumns = this.table.tBodies[0].rows;
+        let rowsLength = this.table.tBodies[0].rows.length;
+        let removeColumnButtonLeft = Collumns[0].cells[0].getBoundingClientRect();
+        let defaultCoordLeft = removeColumnButtonLeft.left + window.pageXOffset;
+        for (let i = 0; i < rowsLength; i++) {
+            if (this.table.tBodies[0].rows[i].cells.length !== 1) {
+                this.removeColumnButton.style.display = 'none';
+                this.removeColumnButton.style.left = defaultCoordLeft + 'px';
+                this.deleteCol(cellIndex, Collumns[i]);
             }
+        }
+
     }
 
-    function RemoveRow(Collumns, rowIndex) {
+    removeRow(rowIndex) {
+        let Collumns = this.table.tBodies[0];
+        let cellsLength = this.table.tBodies[0].rows[0].cells.length;
+        let removeRowButtonTop = Collumns.rows[0].cells[0].getBoundingClientRect();
+        let defaultCoordsTop = removeRowButtonTop.top + window.pageXOffset;
+
+        if (this.table.rows.length !== 1) {
+            this.removeRowButton.style.display = 'none';
+            this.removeRowButton.style.top = defaultCoordsTop + 'px';
+            this.deleteRow(Collumns, rowIndex);
+        }
+    }
+
+    deleteRow(Collumns, rowIndex) {
         return Collumns.deleteRow(rowIndex);
     }
 
-    function removeCol(cellIndex, Collumns) {
+    deleteCol(cellIndex, Collumns) {
         return Collumns.deleteCell(cellIndex);
     }
 
 
-    function addCell(row) {
+    addCell(row) {
         return row.insertCell(-1);
     }
 
-    function addRow(table) {
-        var NewRow = table.insertRow(-1);
-        var ColsLength = table.tBodies[0].rows[0].cells.length;
-        for (var i = 0; i < ColsLength; i++) {
-            addCell(NewRow);
+    addRow() {
+        let NewRow = this.table.insertRow(-1);
+        for (let i = 0; i < this.table.tBodies[0].rows[0].cells.length; i++) {
+            this.addCell(NewRow);
         }
     }
 
-    function addCol(table) {
-        var Rows = table.tBodies[0].rows;
-        var RowsLenght = Rows.length;
-        for (var i = 0; i < RowsLenght; i++) {
-            addCell(Rows[i]);
+    addCol() {
+        let Rows = this.table.tBodies[0].rows;
+        for (let i = 0; i < Rows.length; i++) {
+            this.addCell(Rows[i]);
         }
     }
-    function buttonsPosition(x, y) {
-        _this.removeColumn.style.left = y + 'px';
-        _this.removeRow.style.top = x + 'px';
+
+    buttonsPosition(x, y) {
+        this.removeColumnButton.style.left = y + 'px';
+        this.removeRowButton.style.top = x + 'px';
     }
 }
 
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    var table = new CellTable({'table': '#dynamic'});
+    new CellTable({'table': '.dynamic'});
 });
